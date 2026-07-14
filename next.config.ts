9315e16410d314bfbc5361edad78ff174a678a4d
Raghav Ratnani <raghavdoggy@gmail.com>
2026-07-13 23:17:50 -0400

    Configure automatic GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  typescript: {
+    tsconfigPath: "./tsconfig.pages.json",
+  },
 };
 
 export default nextConfig;
